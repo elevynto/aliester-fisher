529f736c99a77e72b279f01538490e54d725756f
elevynto <dekanifisher@yahoo.co.uk>
2026-05-18 16:43:54 +0100

    cta opens in new tab

--- app/components/CallButton.tsx
+++ app/components/CallButton.tsx
@@ -2,7 +2,7 @@ import { BOOKING_URL } from "../../constants/urls";
 
 export function CallButton({ label = "Book a 30-min call" }: { label?: string }) {
   return (
-    <a className="btn" href={BOOKING_URL}>
+    <a className="btn" href={BOOKING_URL} rel="me noopener" target="_blank">
       {label}
     </a>
   );
